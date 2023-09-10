fc243a6c67ef558d36592f85f1f6d34712b45418
Ahmed Mohamed Abdo <windwave41@yahoo.com>
2023-09-10 12:04:04 -0700

    C0x00

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) 
+/*
+ * Main - Entry Point
+ * A C program that Prints with Puts Func.
+ * Return Must be 0
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
