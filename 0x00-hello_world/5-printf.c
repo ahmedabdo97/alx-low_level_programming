e370fcfc052ff520930a2fc4627bca8e1fa266f0
Ahmed Mohamed Abdo <windwave41@yahoo.com>
2023-09-10 12:18:22 -0700

    C0x00

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C code to use printf function
+ *
+ * Return: 0 (Success)
+ **/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
