e9182a300095b3ae36521397563eea07463ef721
Ahmed Mohamed Abdo <windwave41@yahoo.com>
2023-09-11 17:30:19 -0700

    0x01C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all Alphabet Letters both lower and upper cases
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char ch = 'a';
+	char CH = 'A';
+
+        while (ch <= 'z')
+        {
+                putchar(ch);
+                ch++;
+        }
+
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+        putchar('\n');
+
+        return (0);
+}i
