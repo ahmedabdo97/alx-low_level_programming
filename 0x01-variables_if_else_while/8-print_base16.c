d31462d15e2c66762661f04b547791392d61780c
Ahmed Mohamed Abdo <windwave41@yahoo.com>
2023-09-11 18:08:37 -0700

    0x01C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all numbers at  base 16 in lower case
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchart('\n');
+
+	return (0);
+}
