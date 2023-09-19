b1267019e51a6316837da7f2708e807a3f64bf2b
Ahmed Mohamed Abdo <windwave41@yahoo.com>
2023-09-19 16:22:55 -0700

    CPOINTER

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,10 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * rev_string - This fuction print a string reverser
  *
  *@s: this is the pointer that point to a string
+* Return: Nothingi
  */
 
 void rev_string(char *s)
@@ -11,7 +12,7 @@ void rev_string(char *s)
 	int l, i;
 	char temp;
 
-	for (l = 0; s[1] != '\0'; ++l)
+	for (l = 0; s[l] != '\0'; ++l)
 
 	for (i = 0; i < l / 2; i++)
 	{
